2f9e9a8b341cb1d0e77b55ac8f48c79be12ae544
Stefano Balietti <futur.dorko@gmail.com>
2016-07-13 12:42:01 -0400

    readding nodegame

---
+++ nodegame/conf/servernode.js
@@ -0,0 +1,17 @@
+/**
+ * # servernode.js
+ *
+ * Copyright(c) 2013 Stefano Balietti
+ * MIT Licensed
+ *
+ * Configuration file for ServerNode in nodegame-server.
+ * ---
+ */
+module.exports = configure;
+
+function configure(servernode) {
+    // Nothing extra to configure.
+    // Default configuration set already in:
+    // node_modules/nodegame-server/conf/servernode.js
+    return true;
+}
