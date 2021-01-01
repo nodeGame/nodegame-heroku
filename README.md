867a825caabe69dc387f15f596a25ea9d58dab17
Stefano Balietti <futur.dorko@gmail.com>
2021-01-01 23:42:35 +0100

    fixes for v6

--- README.md
+++ README.md
@@ -3,17 +3,23 @@
 nodeGame ready-to-run on the Heroku cloud.
 
 Read instructions on the nodeGame [wiki](https://github.com/nodeGame/nodegame/wiki/nodeGame-on-Heroku).
- 
+
 
 ## Additional Games in Demo page.
 
 The git repos of all the games on then nodeGame's Heroku [Demo](https://nodegamedemo.herokuapp.com/) page:
 
  - [ultimatum-game](https://github.com/nodeGame/ultimatum-game) (default)
- - [car-sharing](https://github.com/nodeGame/car-sharing) 
+ - [car-sharing](https://github.com/nodeGame/car-sharing)
  - [dictator](https://github.com/nodeGame/dictator)
  - [HawkDove](https://github.com/ewenw/HawkDove)
  - [imgscore](https://github.com/nodeGame/imgscore)
  - [meritocracy-game](https://github.com/nodeGame/meritocracy-game)
  - [prisonersgame](https://github.com/shakty/prisonersgame)
  - [stopgo](https://github.com/nodeGame/stopgo)
+
+See the [Demo Repository](https://nodeGame/nodegamedemo).
+
+## No-Phantoms
+
+Phantom JS testing is currently not available on Heroku.
--- conf/loggers.js
+++ conf/loggers.js
@@ -13,82 +13,82 @@ const winston = require('winston');
 
 // Variable loggers is winston.loggers.
 function configure(loggers, logDir) {
-    let logLevel = winston.level;
-
-    // ServerNode.
-    loggers.add('servernode', {
-        console: {
-            level: logLevel,
-            colorize: true
-        },
-        file: {
-            level: logLevel,
-            timestamp: true,
-            filename: path.join(logDir, 'servernode.log'),
-            maxsize: 1000000,
-            maxFiles: 10
-        }
-    });
-
-    // Channel.
-    loggers.add('channel', {
-        console: {
-            level: logLevel,
-            colorize: true,
-        },
-        file: {
-            level: logLevel,
-            timestamp: true,
-            filename: path.join(logDir, 'channels.log'),
-            maxsize: 1000000,
-            maxFiles: 10
-        }
-    });
-
-    // Messages.
-    // Make custom levels and only File transports for messages.
-    let msgLogger = loggers.add('messages');
-    msgLogger.remove(winston.transports.Console);
-    msgLogger.add(winston.transports.File, {
-        timestamp: true,
-        maxsize: 1000000,
-        filename: path.join(logDir, 'messages.log')
-    });
-
-    // Do not change, or logging might be affected.
-    // Logger.js hardcodes the values for speed.
-    msgLogger.setLevels({
-        // Log none.
-        none: 0,
-        // All DATA msgs.
-        data: 1,
-        // All SET and DATA msgs.
-        set: 3,
-        // All SET, GET and DATA msgs.
-        get: 5,
-        // All SETUP, SET, GET and DATA msgs.
-        setup: 7,
-        // All messages, but **not** PLAYER_UPDATE, SERVERCOMMAND and ALERT.
-        game: 9,
-        // All messages.
-        all: 11
-    });
-
-    // Set default logging level for messages.
-    msgLogger.level = 'all';
-
-    // Clients.
-    loggers.add('clients', {
-        console: {
-            level: logLevel,
-            colorize: true,
-        },
-        file: {
-            level: 'silly',
-            timestamp: true,
-            filename: path.join(logDir, 'clients.log')
-        }
-    });
+    // let logLevel = winston.level;
+    //
+    // // ServerNode.
+    // loggers.add('servernode', {
+    //     console: {
+    //         level: logLevel,
+    //         colorize: true
+    //     },
+    //     file: {
+    //         level: logLevel,
+    //         timestamp: true,
+    //         filename: path.join(logDir, 'servernode.log'),
+    //         maxsize: 1000000,
+    //         maxFiles: 10
+    //     }
+    // });
+    //
+    // // Channel.
+    // loggers.add('channel', {
+    //     console: {
+    //         level: logLevel,
+    //         colorize: true,
+    //     },
+    //     file: {
+    //         level: logLevel,
+    //         timestamp: true,
+    //         filename: path.join(logDir, 'channels.log'),
+    //         maxsize: 1000000,
+    //         maxFiles: 10
+    //     }
+    // });
+    //
+    // // Messages.
+    // // Make custom levels and only File transports for messages.
+    // let msgLogger = loggers.add('messages');
+    // msgLogger.remove(winston.transports.Console);
+    // msgLogger.add(winston.transports.File, {
+    //     timestamp: true,
+    //     maxsize: 1000000,
+    //     filename: path.join(logDir, 'messages.log')
+    // });
+    //
+    // // Do not change, or logging might be affected.
+    // // Logger.js hardcodes the values for speed.
+    // msgLogger.setLevels({
+    //     // Log none.
+    //     none: 0,
+    //     // All DATA msgs.
+    //     data: 1,
+    //     // All SET and DATA msgs.
+    //     set: 3,
+    //     // All SET, GET and DATA msgs.
+    //     get: 5,
+    //     // All SETUP, SET, GET and DATA msgs.
+    //     setup: 7,
+    //     // All messages, but **not** PLAYER_UPDATE, SERVERCOMMAND and ALERT.
+    //     game: 9,
+    //     // All messages.
+    //     all: 11
+    // });
+    //
+    // // Set default logging level for messages.
+    // msgLogger.level = 'all';
+    //
+    // // Clients.
+    // loggers.add('clients', {
+    //     console: {
+    //         level: logLevel,
+    //         colorize: true,
+    //     },
+    //     file: {
+    //         level: 'silly',
+    //         timestamp: true,
+    //         filename: path.join(logDir, 'clients.log')
+    //     }
+    // });
 
     return true;
 }
